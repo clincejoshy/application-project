1ad384259de6782cba8f915ee82a784ed1973576
CLINCE JOSHY <clincepro@gmail.com>
2017-11-19 20:05:58 +0530

    bug fixing

--- project_final/delete_stud.php
+++ project_final/delete_stud.php
@@ -14,8 +14,18 @@ if ($conn->connect_error) {
 //echo "Connected successfully<br>";
 $itemName=$_POST["select"];
 $sqld = "delete from student where student.rollno = $itemName";
+$sqld1 = "delete from sconsumption where sconsumption.rollno = $itemName";
 
 if ($conn->query($sqld) === FALSE){
+    echo "Error: " . $sqld . "<br>" . $conn->error;
+}
+else
+{
+	echo "Roll no ".$itemName." removed successfully";
+	
+}
+
+if ($conn->query($sqld1) === FALSE){
     echo "Error: " . $sqld . "<br>" . $conn->error;
 }
 else
