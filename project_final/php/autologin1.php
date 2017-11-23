e8c99ed4ec7761b37a04cfc8b0c9186c0e7c907d
CLINCE JOSHY <clincepro@gmail.com>
2017-11-23 20:02:53 +0530

    small bug

--- project_final/php/autologin1.php
+++ project_final/php/autologin1.php
@@ -1,9 +1,11 @@
 <?php 
+error_reporting(0);
+ini_set('display_errors', 0);
 	session_start();
 	
-	if($_SESSION["role"]=="admin")
+	if($_SESSION["role"]=="employee")
 	{
-		header("location:../xyz.php");
+		header("location:../billing.php");
 	}
 		
 		else
