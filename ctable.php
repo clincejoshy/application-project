63ede982998636cd1ac6245025702d412f8b735c
CLINCE JOSHY <clincepro@gmail.com>
2018-02-16 23:07:52 +0530

    hostel wise sorting added

--- ctable.php
+++ ctable.php
@@ -25,8 +25,8 @@ $result = $conn->query($sql1);
 $row = $result->fetch_assoc();
 $price=$row["price"];
 $date=$_POST["date"];
-$hostel=$_POST["hostel"];
-$sql="insert into sconsumption values('$rollno','$date','$quantity'*'$price','$name','$itemname','$hostel')";
+//$hostel=$_POST["hostel"];
+$sql="insert into sconsumption values('$rollno','$date','$quantity'*'$price','$name','$itemname')";
 $conn->query($sql);
 }
 $conn->close();
