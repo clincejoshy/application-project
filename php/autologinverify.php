bf4210516c1b613ccbd6ce699ea75c7de0d9df47
Sachin <hellosachindev@gmail.com>
2018-03-15 21:09:38 +0530

    bug

--- php/autologinverify.php
+++ php/autologinverify.php
@@ -3,7 +3,7 @@ error_reporting(0);
 ini_set('display_errors', 0);
 	session_start();
 
-	if($_SESSION["role"]=="student")
+	if($_SESSION["role"]=="employee")
 	{
 		header("location:../verifybill.php");
 	}
