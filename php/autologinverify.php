8f47aca89c56f5fc2516d67e2d180376fa898216
Sachin <hellosachindev@gmail.com>
2018-03-15 21:01:59 +0530

    Bug

--- php/autologinverify.php
+++ php/autologinverify.php
@@ -2,17 +2,7 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 	session_start();
-
-	if($_SESSION["role"]=="employee")
-	{
 		header("location:../verifybill.php");
-	}
-
-		else
-		{
-			echo "404 Not found";
-		}
-
  ?>
  <br/>
  <br/>
