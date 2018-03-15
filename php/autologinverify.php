ac8c04a0adf48a43ccc7e8e93c1ec00fa914d855
Sachin <hellosachindev@gmail.com>
2018-03-15 20:53:12 +0530

    Added Verify

---
+++ php/autologinverify.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(0);
+ini_set('display_errors', 0);
+	session_start();
+		header("location:../verifybill.php");
+
+ ?>
+ <br/>
+ <br/>
+ <br/>
+<br/>
+ <button class="btn"><a class="nav-link" href="../logout1.php">Go To Admin Page!</a></button>
